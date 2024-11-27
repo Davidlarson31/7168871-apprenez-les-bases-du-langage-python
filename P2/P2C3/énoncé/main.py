23206f3851b988bd6566344d82f6db3af4c93a6a
Davidlarson31 <d.larson31@proton.me>
2024-11-27 19:15:53 +0100

    Update main.py

--- P2/P2C3/énoncé/main.py
+++ P2/P2C3/énoncé/main.py
@@ -1,1 +1,18 @@
+def salaire_mensuel(salaire_annuel):
+  return salaire_annuel / 12
+  
+def salaire_hebdomadaire(salaire_mensuel):
+  return salaire_mensuel / 4
+  
+def salaire_horaire(salaire_hebdomadaire, heure_travaillees)
+return salaire_hebdomadaire / heure_travaillees
+
+salaire_annuel = ("Entrez votre salaire annuel: ")
+heures_travailler = ("Entrez le nombre d'heures travaillées par semaine: ")
+
+mensuel = salaire_mensuel(salaire_annuel)
+hebdomadaire = salaire_hebdomadaire(salaire_mensuel)
+horaire = salaire_horaire(salaire_hebdomasaire, heure_travaillees)
+
+print("Votre salaire horaire est de", horaire, "euros.")
 # Ecrivez votre code ici
